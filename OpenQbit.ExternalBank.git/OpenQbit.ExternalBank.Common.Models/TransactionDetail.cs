05d7e6cc11a7849d6870f4790f1f69bffa04f5c3
Maheshika Nishadi <maheshikanishadi7@gmail.com>
2017-05-22 12:40:49 +0530

    Missing Models added

---
+++ OpenQbit.ExternalBank.git/OpenQbit.ExternalBank.Common.Models/TransactionDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.ExternalBank.Common.Models
+{
+    class TransactionDetail
+    {
+        
+        public int Id { set; get; }
+        public int TransactionId { get; set; }  //transation -->fk
+        public int AccountId { get; set; }
+        public double Amount { set; get;}
+        public string Description { get; set; }
+        public virtual Account Account { set; get;}
+    }
+}
