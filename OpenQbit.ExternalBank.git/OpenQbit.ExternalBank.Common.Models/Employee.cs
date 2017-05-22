db2f451480556aa326285f3069e545067fc81733
dinithi <dinithikawmadi@gmail.com>
2017-05-22 15:01:46 +0530

    missing files

---
+++ OpenQbit.ExternalBank.git/OpenQbit.ExternalBank.Common.Models/Employee.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.ExternalBank.Common.Models
+{
+    public class Employee : Person
+    {
+        //public string Address
+        //{
+        //    get
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //    set
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+
+        //public string City
+        //{
+        //    get
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //    set
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+
+        //public string Email
+        //{
+        //    get
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //    set
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+
+        //public string FirstName
+        //{
+        //    get
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //    set
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+
+        //public string LastName
+        //{
+        //    get
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //    set
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+
+        //public string PiD
+        //{
+        //    get
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //    set
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+
+        //public string Tel
+        //{
+        //    get
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //    set
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+    }
+}
---
+++ OpenQbit.ExternalBank.git/OpenQbit.ExternalBank.Common.Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.ExternalBank.Common.Models
+{
+     public class Transaction
+    {
+        public int Id { set; get; }
+        public DateTime TransactionDate { get; set; }
+        public double Amount { get; set; }
+        public int AccountId { get; set; }
+
+        public int TransactionTypeId { get; set; }
+        public virtual TransactionType TransactionType { get; set;}
+
+
+    }
+}
