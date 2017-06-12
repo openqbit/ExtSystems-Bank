0acbec05a8a4417e5d5922360252695cac7bec3d
malki dinushika <malki.dinushika2@gmail.com>
2017-06-12 15:11:54 +0530

    Done

---
+++ OpenQbit.ExternalBank.git/OpenQbit.ExternalBank.DAL/ExternalBankContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+using OpenQbit.ExternalBank.Common.Models;
+using OpenQbit.ExternalBank.DAL.Contracts;
+
+namespace OpenQbit.ExternalBank.DAL
+{
+   public class ExternalBankContext : DbContext 
+    {
+        public ExternalBankContext() : base("ExternalBankDB")
+        {
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public DbSet<Account> Account { get; set; }
+
+        public DbSet<AccountTypes> AccountTypes { get; set; }
+
+        public DbSet<Customer> Customer { get; set; }
+
+        public DbSet<Employee> Employee { get; set; }
+
+        public DbSet<Person> Person { get; set; }
+
+        public DbSet<Transaction> Transaction { get; set; }
+
+        public DbSet<TransactionDetail> TransactionDetail { get; set; }
+
+        public DbSet<TransactionType> TransactionType { get; set; }
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
